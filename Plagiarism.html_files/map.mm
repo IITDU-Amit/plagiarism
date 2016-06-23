1921d567a02d019c0cc8281abed29bfc27a5ea05
Amit Seal Ami <lordamit@gmail.com>
2016-06-23 23:10:41 +0600

    new

---
+++ Plagiarism.html_files/map.mm
@@ -0,0 +1,59 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1466692177213" ID="ID_1433517990" MODIFIED="1466693763815" TEXT="Plagiarism Policy">
+<node COLOR="#ff6666" CREATED="1466692238685" ID="ID_1317466363" MODIFIED="1466697882031" POSITION="right" TEXT="This document is based specifically on">
+<node COLOR="#ff6666" CREATED="1466692263629" ID="ID_181101825" MODIFIED="1466697815969" TEXT="MIT Academic Integrity Handbook - http://web.mit.edu/academicintegrity/handbook/handbook.pdf"/>
+<node COLOR="#ff6666" CREATED="1466692327828" ID="ID_374578904" MODIFIED="1466697838010" TEXT="What is Plagiarism - from Stanford Office of Community Standards - https://communitystandards.stanford.edu/student-conduct-process/honor-code-and-fundamental-standard/additional-resources/what-plagiarism"/>
+<node COLOR="#ff6666" CREATED="1466692350246" ID="ID_182887259" MODIFIED="1466697826575" TEXT=" Honor Code and Fundamental Standard  from Stanford Offce of Community Standards - http://csmajor.stanford.edu/HonorCode.shtml"/>
+<node COLOR="#ff6666" CREATED="1466693428996" ID="ID_1011725571" MODIFIED="1466696029115" TEXT="Information last updated from above mentioned sources - June 23, 2016"/>
+</node>
+<node CREATED="1466693076215" ID="ID_181237444" MODIFIED="1466697872391" POSITION="left" TEXT="Honor Code">
+<node CREATED="1466693481103" FOLDED="true" ID="ID_734022187" MODIFIED="1466697875112" TEXT="Plagiarism">
+<node CREATED="1466693379427" ID="ID_1750079709" MODIFIED="1466693386711" TEXT="Trust value of own intellect"/>
+<node CREATED="1466693527294" ID="ID_1623653206" MODIFIED="1466693536888" TEXT="Understake research honestly and credit others for their work"/>
+<node CREATED="1466693495325" ID="ID_276835704" MODIFIED="1466693501504" TEXT="Don&apos;t purchase papers "/>
+<node CREATED="1466693501881" ID="ID_863430427" MODIFIED="1466693506676" TEXT="Don&apos;t have someone write a paper for you"/>
+<node CREATED="1466693540408" ID="ID_1119151082" MODIFIED="1466693548681" TEXT="Don&apos;t copy ideas, data or exact working without citing your source"/>
+</node>
+<node CREATED="1466693552710" FOLDED="true" ID="ID_225143152" MODIFIED="1466697875649" TEXT="Unauthorized Collaboration">
+<node CREATED="1466693558726" ID="ID_1545561627" MODIFIED="1466693562296" TEXT="Do your own thinking"/>
+<node CREATED="1466693562834" ID="ID_50652323" MODIFIED="1466693576129" TEXT="Don&apos;t collaboratte with another student beyond the extent specifically approved by the instructor"/>
+</node>
+<node CREATED="1466693579040" FOLDED="true" ID="ID_1810070217" MODIFIED="1466697876147" TEXT="Cheating">
+<node CREATED="1466693582249" ID="ID_1720228683" MODIFIED="1466693587800" TEXT="Demonstrate your own achievement"/>
+<node CREATED="1466693622714" ID="ID_975811937" MODIFIED="1466693625449" TEXT="Do original Work"/>
+<node CREATED="1466693625899" ID="ID_696432554" MODIFIED="1466693636418" TEXT="Do not submit project or papers that have been done for a previous class"/>
+</node>
+<node CREATED="1466693643962" FOLDED="true" ID="ID_1725505966" MODIFIED="1466697876752" TEXT="Facilitating Academic Dishonesty">
+<node CREATED="1466693650930" ID="ID_1297649781" MODIFIED="1466693654611" TEXT="Showcase your abiliities"/>
+<node CREATED="1466693655063" ID="ID_1826337052" MODIFIED="1466693664384" TEXT="Do not allow another student to copy your work"/>
+</node>
+</node>
+<node CREATED="1466693357591" ID="ID_576932509" MODIFIED="1466697884117" POSITION="right" TEXT="Definining Plagiarism">
+<node CREATED="1466693696023" ID="ID_8915573" MODIFIED="1466693723650" TEXT="Plagiarism occurs whe you use another&apos;s words, ideas, assertions, data, figures and do not acknowledge that you have done so."/>
+</node>
+<node CREATED="1466693950122" ID="ID_1954857461" MODIFIED="1466697877630" POSITION="left" TEXT="Avoiding Plagiarism">
+<node CREATED="1466693966533" FOLDED="true" ID="ID_1335336036" MODIFIED="1466697879398" TEXT="For Program / Project / Code">
+<node CREATED="1466693984957" ID="ID_731078277" MODIFIED="1466693997757" TEXT="1. You must not look at solutions or program code that are not your own"/>
+<node CREATED="1466694035443" ID="ID_453627473" MODIFIED="1466694043718" TEXT="2. You must not share your solution code with other students."/>
+<node CREATED="1466694053878" ID="ID_572387128" MODIFIED="1466694094168" TEXT="3. Take reasonable measures to maintain the privacy of your solutions. "/>
+<node CREATED="1466694096217" ID="ID_1741227496" MODIFIED="1466694097845" TEXT="4.Do not leave copies of your work on public computers nor post your solution code on a public website."/>
+<node CREATED="1466694099957" ID="ID_1485773825" MODIFIED="1466694151315" TEXT="5. You must indicate on your submission any assistance you received."/>
+<node CREATED="1466694162640" ID="ID_1313409810" MODIFIED="1466696207099" TEXT="6. Discuss Ideas together, but do the coding on your own."/>
+<node CREATED="1466696207987" ID="ID_1474987280" MODIFIED="1466696219209" TEXT="Give license attribution for used open source code in project"/>
+</node>
+<node CREATED="1466693961068" FOLDED="true" ID="ID_1875539803" MODIFIED="1466697880064" TEXT="For Literature">
+<node CREATED="1466694978889" ID="ID_1962594159" MODIFIED="1466696332231" TEXT="In the body of your paper, show where the words or information came from using an appropriate format (quotation marks or indent long quotations)"/>
+<node CREATED="1466695007854" ID="ID_1973450376" MODIFIED="1466695080139" TEXT="Provide complete information about the source at the end of your paper, in bibliography / references. It should include, but not limited to: ">
+<node CREATED="1466695031281" ID="ID_1144845808" MODIFIED="1466695034630" TEXT="Author"/>
+<node CREATED="1466695035471" ID="ID_1821701348" MODIFIED="1466695036428" TEXT="Title"/>
+<node CREATED="1466695036863" ID="ID_952789797" MODIFIED="1466695039353" TEXT="Name of publication"/>
+<node CREATED="1466695039654" ID="ID_1015193886" MODIFIED="1466695040461" TEXT="Date"/>
+</node>
+</node>
+<node CREATED="1466694959451" FOLDED="true" ID="ID_914411037" MODIFIED="1466697880479" TEXT="For Presentation">
+<node CREATED="1466694965448" ID="ID_93947063" MODIFIED="1466694977551" TEXT="Acknowledhe on your slide where the graph, chart or other information came from."/>
+</node>
+</node>
+</node>
+</map>
